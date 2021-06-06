3d453abc0b58bd5a822b6a25ffc7cdeae9b7bc97
Aviral3888 <63301012+Aviral3888@users.noreply.github.com>
2021-06-06 12:37:53 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Note />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js </h1>
+      <p>
+        This was an amazing bootcamp taken up by Shaurya Sinha Sir. We covered
+        everything from scratch.
+      </p>
+      Technologies Used:
+      <br />
+      React, Javascript, HTML
+    </div>
+  );
+}
+
+export default Note;
